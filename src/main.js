263ff3d6f54a00941f5fb848666604b5aecc0f2e
haijiaoLIU <1079525567@qq.com>
2021-11-26 17:18:23 +0800

    基础框架

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
-
+import store from './store'
 Vue.config.productionTip = false
-
+// 添加事件总线事件
+// Vue.prototype.$bus = new Vue()
 new Vue({
   render: h => h(App),
+  store,
   router
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuex from 'vuex'
+//1. 安装插件
+Vue.use(Vuex)
+//2.创建store对象
+const store = new Vuex.Store({
+  state:{},
+  mutations:{}
+})
+// 3.挂载Vue实例上
+export default store
+
